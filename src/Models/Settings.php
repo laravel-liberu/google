922dda11ce0e1bfea2ee99d36584d7794a30d9eb
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2022-10-10 13:35:32 +0300

    removes  static property from settings model

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -18,18 +18,15 @@ class Settings extends Model
     protected $guarded = ['id'];
 
     protected $casts = [
-        'maps_key'         => Encrypt::class,
-        'recaptcha_key'    => Encrypt::class,
+        'maps_key' => Encrypt::class,
+        'recaptcha_key' => Encrypt::class,
         'recaptcha_secret' => Encrypt::class,
-        'analytics_id'     => Encrypt::class,
+        'analytics_id' => Encrypt::class,
     ];
-
-    private static $instance;
 
     public static function current()
     {
-        return self::$instance
-            ??= self::cacheGet(1)
+        return self::cacheGet(1)
             ?? self::factory()->create();
     }
 
